f0ec2bd3f9064cd75b5c748b987193b9fce25cf8
Jenny from the Block <sunflowermaster234@yahoo.com>
2023-01-24 01:10:10 +0200

    menu mvp completed

--- client/src/components/pages/GeneralDining.js
+++ client/src/components/pages/GeneralDining.js
@@ -1,8 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./GeneralDining.css";
+import { get } from "../../utilities";
 
 const GeneralDining = () => {
-  return <div>General Dining</div>;
+  const [diningSettings, setdiningSettings] = useState({
+    chosen: [null, null, null],
+    rankings: ["Next", "Simmons", "Maseeh", "McCormmick", "New Vassar", "Baker"],
+  });
+  const [menus, setMenus] = useState({
+    Next: {
+      breakfast: [{ dishName: "potato" }, { dishName: "ji" }],
+      lunch: [{ dishName: "lunhc" }],
+      dinner: [{ dishName: "dinner" }],
+      lateNight: [{ dishName: "late night" }],
+    },
+    Simmons: { breakfast: [{ dishName: "potato" }], lunch: [], dinner: [], lateNight: [] },
+    Maseeh: { breakfast: [{ dishName: "potato" }], lunch: [], dinner: [], lateNight: [] },
+    McCormmick: { breakfast: [{ dishName: "potato" }], lunch: [], dinner: [], lateNight: [] },
+    NewVassar: { breakfast: [{ dishName: "potato" }], lunch: [], dinner: [], lateNight: [] },
+    Baker: { breakfast: [{ dishName: "potato" }], lunch: [], dinner: [], lateNight: [] },
+  });
+  const [meal, setMeal] = useState("breakfast");
+
+  const loadDiningSettings = () => {
+    get("/api/dining-settings").then((settings) => {
+      setdiningSettings({
+        chosen: settings.chosen,
+        rankings: settings.rankings,
+      });
+    });
+  };
+
+  const loadMenu = () => {
+    get("/api/menus").then((menus) => {
+      setMenus(menus);
+    });
+  };
+
+  const loadMeal = () => {
+    const date = new Date();
+    const hour = date.getUTCHours();
+    if (hour >= 16 && hour <= 20) {
+      setMeal("lunch");
+    } else if (hour >= 22 || hour < 2) {
+      setMeal("dinner");
+    } else if (hour >= 2 || hour <= 6) {
+      setMeal("lateNight");
+    } else {
+      setMeal("breakfast");
+    }
+  };
+
+  const changeMeal = (val) => {
+    setMeal(val);
+  };
+
+  useEffect(() => {
+    // loadDiningSettings();
+    // loadMenu();
+    loadMeal();
+  }, []);
+
+  return (
+    <div>
+      <div className="center">
+        <h1>Select the meal to view the meal</h1>
+        <select value={meal} onChange={(event) => changeMeal(event.target.value)}>
+          <option value="breakfast">breakfast</option>
+          <option value="lunch">lunch</option>
+          <option value="dinner">dinner</option>
+          <option value="lateNight">late night</option>
+        </select>
+      </div>
+      <br></br>
+      <div className="wrapper">
+        {diningSettings.rankings.map((item) => {
+          return (
+            <div className="dining-container">
+              <h1>{item}</h1>
+              <div className="menu">
+                {menus[item.replaceAll(" ", "")][meal].map((meal) => {
+                  return <p>{meal.dishName}</p>;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default GeneralDining;
--- client/src/components/pages/SharedDining.js
+++ client/src/components/pages/SharedDining.js
@@ -3,64 +3,7 @@ import "./SharedDining.css";
 import { get } from "../../utilities";
 
 const SharedDining = (props) => {
-  const [diningSettings, setdiningSettings] = useState({
-    chosen: [null, null, null],
-    rankings: ["Next", "Simmons", "Maseeh", "McCormmick", "New Vassar", "Baker"],
-  });
-  const [menus, setMenus] = useState({
-    Next: { breakfast: [], lunch: [], dinner: [] },
-    Simmons: { breakfast: [], lunch: [], dinner: [] },
-    Maseeh: { breakfast: [], lunch: [], dinner: [] },
-    McCormmick: { breakfast: [], lunch: [], dinner: [] },
-    New_Vassar: { breakfast: [], lunch: [], dinner: [] },
-    Baker: { breakfast: [], lunch: [], dinner: [] },
-  });
-
-  const loadDiningSettings = () => {
-    get("/api/dining-settings").then((settings) => {
-      setdiningSettings({
-        chosen: settings.chosen,
-        rankings: settings.rankings,
-      });
-    });
-  };
-
-  useEffect(() => {
-    loadDiningSettings();
-  }, []);
-
-  const loadMenu = () => {
-    get("/api/menus").then((menus) => {
-      setMenus(menus);
-    });
-  };
-
-  useEffect(() => {
-    loadMenu();
-  }, []);
-
-  return (
-    <div>
-      <div className="center">
-        <h1>Select the meal to view the meal</h1>
-        <select>
-          <option value="breakfast">breakfast</option>
-          <option value="lunch">lunch</option>
-          <option value="dinner">dinner</option>
-        </select>
-      </div>
-      <br></br>
-      <div className="wrapper">
-        {diningSettings.rankings.map((item) => {
-          return (
-            <div className="dining-container">
-              <h1>{item}</h1>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <p>hi</p>;
 };
 
 export default SharedDining;
