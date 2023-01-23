5ffb6d4c3809e809ea7900b8a3241041d695871a
Jovan Markovic <jovanmarkovic888777@gmail.com>
2023-01-23 20:48:04 +0100

    msg

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./Profile.css";
 
 const Profile = () => {
-  return <div>I successfully linked this</div>;
+  /// Use flex to make picture and data nicer
+  /// Picture link apparently not working because need to load from API
+  return (
+    <div>
+      <div class="container">
+        <img src="/pic.jpg" alt="profilePic"/>
+
+        <ul>
+          <li>Name Surname</li>
+          <li>Pronouns</li>
+          <li>Kerb + Contact Info</li>
+          <li>Class Year</li>
+          <li>Major</li>
+        </ul>
+      </div>
+
+      <h2>
+        Mutual Friends
+      </h2>
+    </div>
+  );
 };
 
 export default Profile;
