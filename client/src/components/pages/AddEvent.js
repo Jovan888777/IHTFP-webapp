a7dc7e15aea5bed6facba9ad0c2dcbaf090cf909
Jovan Markovic <jovanmarkovic888777@gmail.com>
2023-01-25 19:14:46 +0100

    design for add event

--- client/src/components/pages/AddEvent.js
+++ client/src/components/pages/AddEvent.js
@@ -76,84 +76,118 @@ const AddEvent = (props) => {
   };
 
   return (
-    <div>
+    <div className="center">
       <div className="center" hidden={!done}>
         Successfully added your event!
       </div>
-      <div className="center" hidden={done}>
+      <div className="addCard center" hidden={done}>
         {/* {done && <Navigate to="/my-events" replace={true} />} */}
         <h1>Add Event</h1>
-        Name:{" "}
-        <input
-          name="eventname"
-          type="textbox"
-          placeholder="Enter some text"
-          value={eventName}
-          onChange={(event) => setEventName(event.target.value)}
-          required
-        />
-        <br></br>
-        Group:
-        <input
-          name="group"
-          type="textbox"
-          placeholder="Enter some text"
-          value={eventGroup}
-          onChange={(event) => setEventGroup(event.target.value)}
-          required
-        />
-        <br></br>
-        Location:
-        <input
-          name="location"
-          type="textbox"
-          placeholder="Enter some text"
-          value={eventLocation}
-          onChange={(event) => setEventLocation(event.target.value)}
-          required
-        />
-        <br></br>
-        Description:
-        <textarea
-          name="description"
-          placeholder="Enter some text"
-          value={eventDescription}
-          onChange={(event) => setEventDescription(event.target.value)}
-          required
-        ></textarea>
-        <br></br>
-        Start Date and Time:
-        <input
-          type="datetime-local"
-          name="start"
-          min={new Date().toISOString().slice(0, new Date().toISOString().lastIndexOf(":"))}
-          max="2023-12-31T23:59"
-          value={eventStart}
-          onChange={(event) => setEventStart(event.target.value)}
-          required
-        />
-        <br></br>
-        End Date and Time:
-        <input
-          type="datetime-local"
-          name="start"
-          min={eventStart}
-          max="2023-12-31T23:59"
-          value={eventEnd}
-          onChange={(event) => setEventEnd(event.target.value)}
-          required
-        />
-        <br></br>
-        Guest List Needed:{" "}
-        <input
-          name="guestlistneeded"
-          type="checkbox"
-          value={eventGuestlistNeeded}
-          onChange={() => setEventGuestlistNeeded(!eventGuestlistNeeded)}
-        />
-        <br></br>
-        <input value="Add" type="submit" onClick={postNewEvent} />
-        <div>{err}</div>
+        <div className="inputs">
+          <div className="halfWidth">
+            Name:
+          </div>
+          <div className="halfWidth">
+            <input
+              className="inputBox"
+              name="eventname"
+              type="textbox"
+              placeholder="Enter some text"
+              value={eventName}
+              onChange={(event) => setEventName(event.target.value)}
+              required
+            />
+          </div>
+          <div className="halfWidth">
+            Group:
+          </div>
+          <div className="halfWidth">
+            <input
+              className="inputBox"
+              name="group"
+              type="textbox"
+              placeholder="Enter some text"
+              value={eventGroup}
+              onChange={(event) => setEventGroup(event.target.value)}
+              required
+            />
+          </div>
+          <div className="halfWidth">
+            Location:
+          </div>
+          <div className="halfWidth">
+            <input
+              className="inputBox"
+              name="location"
+              type="textbox"
+              placeholder="Enter some text"
+              value={eventLocation}
+              onChange={(event) => setEventLocation(event.target.value)}
+              required
+            />
+          </div>
+          <div className="halfWidth">
+          Description:
+          </div>
+          <div className="halfWidth">
+            <textarea
+              className="inputBox"
+              name="description"
+              placeholder="Enter some text"
+              value={eventDescription}
+              onChange={(event) => setEventDescription(event.target.value)}
+              required
+            ></textarea>
+          </div>
+          <div className="halfWidth">
+          Start Date and Time:
+          </div>
+          <div className="halfWidth">
+          <input
+            className="inputBox"
+            type="datetime-local"
+            name="start"
+            min={new Date().toISOString().slice(0, new Date().toISOString().lastIndexOf(":"))}
+            max="2023-12-31T23:59"
+            value={eventStart}
+            onChange={(event) => setEventStart(event.target.value)}
+            required
+          />
+          </div>
+          <div className="halfWidth">
+            End Date and Time:
+          </div>
+          <div className="halfWidth">
+            <input
+              className="inputBox"
+              type="datetime-local"
+              name="start"
+              min={eventStart}
+              max="2023-12-31T23:59"
+              value={eventEnd}
+              onChange={(event) => setEventEnd(event.target.value)}
+              required
+            />
+          </div>
+          <div className="halfWidth">
+            Guest List Needed:
+          </div>
+          <div className="halfWidth">
+            <input
+              className="inputBox"
+              name="guestlistneeded"
+              type="checkbox"
+              value={eventGuestlistNeeded}
+              onChange={() => setEventGuestlistNeeded(!eventGuestlistNeeded)}
+            />
+          </div>
+          <div className="fullWidth">
+            <input className ="inputBox" value="Add" type="submit" onClick={postNewEvent} />
+          </div>
+          <div className="fullWidth">
+            {err}
+          </div>
+        </div>
       </div>
     </div>
   );
