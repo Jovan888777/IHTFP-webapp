461e5d941bd4787110e1a5f33b1a800b7cf0d31e
Jenny from the Block <sunflowermaster234@yahoo.com>
2023-01-29 20:55:26 +0200

    guestlist can now be downloaded by event creators from my events page

--- client/src/components/pages/MyEvents.js
+++ client/src/components/pages/MyEvents.js
@@ -18,6 +18,25 @@ const MyEvents = (props) => {
     });
   };
 
+  const downloadGuestlist = (eventId) => {
+    get("/api/event-guestlist", { eventId: eventId })
+      .then((guests) => {
+        if (guests) {
+          const file = new Blob([guests.join("\n")], { type: "text/plain" });
+          const element = document.createElement("a");
+          element.href = URL.createObjectURL(file);
+          element.download = "guestlist_" + eventId + ".txt";
+          document.body.appendChild(element); // Required for this to work in FireFox
+          element.click();
+        } else {
+          alert("There is no one on the guestlist yet!");
+        }
+      })
+      .catch((err) => {
+        console.log(`failed to get all event guestlist:${err}`);
+      });
+  };
+
   useEffect(() => {
     loadMyEvents();
   }, []);
@@ -29,7 +48,13 @@ const MyEvents = (props) => {
       {myEvents.map((element) => (
         <div>
           <EventDisplay {...element} userId={props.userId} />
-
+          {element.guestlistNeeded ? (
+            <button onClick={(e) => downloadGuestlist(e.target.className)} className={element._id}>
+              Download Guestlist
+            </button>
+          ) : (
+            ""
+          )}
           <div className="column">
             <button
               onClick={() => {
